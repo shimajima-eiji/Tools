9409870c5d2d4ae4467b52543910e4e5b5e013f4
のむら屋ごろう <shimajima-eiji@live.jp>
2020-12-17 14:16:30 +0900

    Update call_husband.cpp

--- M5stickC/call_husband.cpp
+++ M5stickC/call_husband.cpp
@@ -1,19 +1,22 @@
-/*
-# 使い方
-育児中の家庭を想定
-リモートワークで働いていると、子供のお世話が結局ワンオペになるので気軽に呼べる機能をM5StickCを使って作成
-トラブル発生とご飯の時ぐらいしか呼ばれないので、とりあえず呼ばれれば応答できる仕組みを整える。
-
-
-
-事前に
-- M5StickCにビルドできる環境
-- LINE通知を有効にする設定
-- LINE Notifyのトークン
-が必要
-
-# 
-*/
+/**
+ * # 使い方
+ * 育児中の家庭を想定しているが、手が離せない時に決まったメッセージをLINEで送る時なら何でも使える
+ * リモートワークで働いていると、子供のお世話が結局ワンオペになるので気軽に呼べる機能をM5StickCを使って作成
+ * 今のところトラブル発生とご飯の時ぐらいしか呼ばれないので、応答できる仕組みを整える
+ * 
+ * # 事前準備
+ * - M5StickCにビルドできる環境：著者はVSCode + PlatformIO IDEを使用。Macbookだと出来ない？
+ * - LINE通知を有効にする設定
+ * - LINE Notifyのトークン： https://notify-bot.line.me/my/
+ * が必要。
+ * 
+ * 当然、M5StickCも必要。
+ * amazonで安く買える。
+ * https://amzn.to/2Ks7tZD
+ * 
+ * # 変更箇所
+ * 変更エリアを後述のコメントで案内しているので、該当するものを入れていく。
+ **/
 
 #include <ssl_client.h>
 #include <WiFiClientSecure.h>
@@ -23,9 +26,11 @@
 #include "M5StickC.h"
 #include "M5Display.h"
 
+/* 変更エリア　ここから */
+
 const char *ssid = "Your SSID(2.4G)";
 const char *password = "Your SSID pass";
-const char *token = "Your LINE Notify Token";  // https://notify-bot.line.me/my/
+const char *token = "Your LINE Notify Token";
 
 // [M5]ボタンの表示設定
 const char *btna_line_message = "LINEに送るメッセージ。日本語可。サンプルでは「ちょっと来て」";
@@ -34,6 +39,8 @@ const char *btna_display_message = "送信状況をM5stickで表示。英語の�
 // サイドボタンの表示設定
 const char *btnb_line_message = "LINEに送るメッセージ。日本語可。サンプルでは「ご飯できたよ」。要らなさそう";
 const char *btnb_display_message = "送信状況をM5stickで表示。英語のみ。「gohan」";
+
+/* 変更エリア　ここまで */
 
 void view(String message)
 {
